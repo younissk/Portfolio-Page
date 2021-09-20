956d811c89c4b857f75e9784afea1acfda17a9c5
younissk <younisskandah@gmail.com>
2021-09-20 14:42:16 +0200

    updated design and functionality

---
+++ script.js
@@ -0,0 +1,22 @@
+const background_project = document.getElementById("project-preview");
+
+const project_links = [
+  "https://younissk.github.io/p5-projects/Bezier/index.html",
+  "https://younissk.github.io/p5-projects/Maurer%20curve/index.html",
+  "https://younissk.github.io/p5-projects/Mandelbrot/index.html",
+  "https://younissk.github.io/p5-projects/Occilation/index.html",
+  "https://younissk.github.io/p5-projects/Snowflake/index.html",
+];
+
+let i = 0;
+background_project.src = project_links[i];
+i++;
+
+setInterval(() => {
+  if (i < project_links.length) {
+    background_project.src = project_links[i];
+    i++;
+  } else {
+    i = 0;
+  }
+}, 10000);

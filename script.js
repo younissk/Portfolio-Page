a1f678962b6449d54d78f54252a4b088a737c95d
younissk <younisskandah@gmail.com>
2021-10-11 18:05:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const background_project = document.getElementById("project-preview");
 
 const project_links = [
+  "https://preview.p5js.org/Youniss/present/9ZHeqNZ0j",
   "https://younissk.github.io/p5-projects/Self%20avoiding%20walk/index.html",
+  "https://preview.p5js.org/Youniss/present/csa3k0oP4",
   "https://younissk.github.io/p5-projects/Bezier/index.html",
   "https://younissk.github.io/p5-projects/Maurer%20curve/index.html",
   "https://younissk.github.io/p5-projects/Mandelbrot/index.html",
